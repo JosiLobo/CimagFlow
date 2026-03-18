d18f58d46b340fb2fa353583261e347444fb3594
JosiLobo <josiana.lobo@qode7.com>
2026-03-18 20:55:25 -0300

    Update pastas-client.tsx
    Ajustes Gestor

--- app/(app)/pastas/_components/pastas-client.tsx
+++ app/(app)/pastas/_components/pastas-client.tsx
@@ -298,6 +298,7 @@ export default function PastasClient() {
                           </div>
                         </div>
                       )}
+                      </div>
                   </motion.div>
                 ))}
               </div>
